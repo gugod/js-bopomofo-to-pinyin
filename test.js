dde2e923a934a3bff2a329d46577bf3ef01f4070
Kang-min Liu <gugod@gugod.org>
2015-08-10 22:53:32 +0800

    convert with a full table

--- test.js
+++ test.js
@@ -4,6 +4,7 @@ var test = require('tape');
 var bpmf = require("./bopomofo-to-pinyin.js");
 
 test('trivial cases', function(t) {
-    t.plan(1);
+    t.plan(2);
     t.equal(bpmf.bopomofoToPinyin("ㄎㄚ"), "ka");
+    t.equal(bpmf.bopomofoToPinyin("ㄈㄟ"), "fei");
 });
